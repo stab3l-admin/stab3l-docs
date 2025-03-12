23b615796a9985c3e5c3e4ff4d52bc6a35e039a2
stab3l-admin <admin@stab3l.com>
2025-03-12 15:01:08 -0600

    Fix ChartType export in tokenomics playground components

--- src/components/ui/tokenomics-playground/components/index.ts
+++ src/components/ui/tokenomics-playground/components/index.ts
@@ -4,4 +4,4 @@ export { default as ForecastingEngine } from './ForecastingEngine';
 export { default as SensitivityAnalysis } from './SensitivityAnalysis';
 export { default as ScenarioSelector } from './ScenarioSelector';
 export { default as LightweightChart } from './LightweightChart';
-export type { ChartData, ChartType } from './LightweightChart'; 
+export type { ChartData, CandlestickChartData } from './LightweightChart'; 
